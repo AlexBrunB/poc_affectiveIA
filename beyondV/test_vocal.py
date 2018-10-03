de566ff4ec1e54afccc7f7abb8982afa10ddff40
Alain BERRIER <alain.berrier@outlook.com>
2018-10-03 16:16:24 +0200

    Add POC for AffectiveIA company

---
+++ beyondV/test_vocal.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+** POC Affective IT in ~/projects/beyondverbal/
+** 
+** Made by Alexandre Brun
+** Login   <alexandre.brun@suricats-consulting.com>
+** 
+** Started on  Wed Oct 03 10:28:11 2017 Alexandre Brun
+"""
+
+import requests
+requests.packages.urllib3.disable_warnings()
+
+
+"""
+Méthode pour call l'API Beyond Verbal et soumettre un extrait sonore à étude.
+1er param: clé API
+2e  param : Path de l'extrait audio
+"""
+def getAnalysis(API_Key,WavPath):
+
+    res = requests.post("https://token.beyondverbal.com/token",data={"grant_type":"client_credentials","apiKey":API_Key})
+    token = res.json()['access_token']
+    headers={"Authorization":"Bearer "+token}
+
+    pp = requests.post("https://apiv4.beyondverbal.com/v4/recording/start",json={"dataFormat": { "type":"WAV" }},verify=False,headers=headers)
+    if pp.status_code != 200:
+        print(pp.status_code, pp.content)
+        return
+    recordingId = pp.json()['recordingId']
+    with open(WavPath,'rb') as wavdata:
+        r = requests.post("https://apiv4.beyondverbal.com/v4/recording/"+recordingId,data=wavdata, verify=False, headers=headers)
+        return r.json()
+
+
+json = getAnalysis("API_Key","WavPath")
+print(json)
